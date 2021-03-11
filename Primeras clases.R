f8d84cccf501d772b7aa21cb1cabf6b08115bb0e
LIZZETH GOMEZ <lizzethgomezrodriguez@ciencias.unam.mx>
2021-03-10 21:31:54 -0600

    Update

--- Primeras clases.R
+++ Primeras clases.R
@@ -3,10 +3,10 @@
     # Clase de Inferencia, Intro a R
     # =========================================================================
     
-    # R usa tipado dinámico, que es cuando modificamos las variables asignadas
-    # Número de Filas nrow()
-    # Número de Columnas ncol()
-    # Número de variables length()
+    # R usa tipado din?mico, que es cuando modificamos las variables asignadas
+    # Numero de Filas nrow()
+    # Numero de Columnas ncol()
+    # Numero de variables length()
     # Conocer la estructura de los datos str()
     # arrja tipo de datos, num de obs etc.
     
@@ -30,13 +30,13 @@
     var(base$Superficie)
     sd(base$Superficie)
     
-    # En box plot, la base del rectángulo es el percentil 0.25, la línea es el 0.05
-    # y el techo sería el 0.75
+    # En box plot, la base del rect?ngulo es el percentil 0.25, la l?nea es el 0.05
+    # y el techo ser?a el 0.75
     boxplot(base$Superficie)
     
     
     # Lunes 06 de Marzo 2021
-    # Gráfico
+    # Gr?fico
     boxplot(base$Superficie, ylim = c(0,300))
     abline(h = quantile(base$Superficie,0.25))
     abline(h = quantile(base$Superficie,0.75), col = 2)
@@ -46,7 +46,7 @@
     psych::skew(base$Superficie)
     
     
-    # Calcular manualmente la Simetría
+    # Calcular manualmente la Simetr?a
     x <- base$Superficie
     n <- length(x)
     sum((x-mean)^{3})/(n*sd(x)^{3})
@@ -54,8 +54,8 @@
     
     # Restamos al vector x el promedio,entrada por entrada
     
-    # Gráfico de Histograma
-    # Toma el valor mínimo y máximo, lo divide en partes iguales
+    # Grafico de Histograma
+    # Toma el valor minimo y maximo, lo divide en partes iguales
     # y ubica cuantas observaciones caen en cada intervalo.
     hist(base$Superficie)
     hist(base$Superficie, breaks = 50)
@@ -66,12 +66,12 @@
     base<- readxl::read_excel("BASE.xlsx")
     
     hist(base$Superficie, freq = FALSE, breaks = 50)
-    # Graficamos un modelo normal atrás de la curva:
+    # Graficamos un modelo normal atr?s de la curva:
     # dnorm me da la densidad del modelo que queremos graficar
     # En este caso es la normal (x,media, varianza)
     curve(dnorm(x,100,10), col = 2, add = TRUE) #Centramos la media en 100 con varianza 10
     
-    plot(ecdf(base$Superficie)) #ecdf es la función F(x)
+    plot(ecdf(base$Superficie)) #ecdf es la funci?n F(x)
     
     #Simulamos observaciones random del modelo normal
     muestra <- rnorm(100, 0,1) # 100 observaciones, media=0, var = 1
@@ -84,9 +84,9 @@
     curve(pnorm(x,0,1), col = 2, add = TRUE, lwd = 2)
     
     # TEOREMA DE GILVENKO-CANTELI
-    #Vamos aproximandonos a la función distribución empírica agrandando la muestra
+    #Vamos aproximandonos a la funci?n distribuci?n emp?rica agrandando la muestra
     muestra <- rnorm(1000, 0,1) # 100 observaciones, media=0, var = 1
-    plot(ecdf(muestra)) #Práticamente es la función de distribución empírica
+    plot(ecdf(muestra)) #Pr?ticamente es la funci?n de distribuci?n emp?rica
     curve(pnorm(x,0,1), col = 2, add = TRUE, lwd = 2)
     
     
